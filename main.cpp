e09e21a2eca849526a172f36fa6c8176f1cab04f
Alec Jacobson <alecjacobson@gmail.com>
2017-03-15 23:52:48 -0400

    with png

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
+#include "include/angle_defect.h"
+#include "include/principal_curvatures.h"
+#include "include/mean_curvature.h"
+#include <igl/avg_edge_length.h>
+#include <igl/png/writePNG.h>
+#include <igl/next_filename.h>
 #include <igl/read_triangle_mesh.h>
 #include <igl/parula.h>
+#include <igl/doublearea.h>
 #include <igl/viewer/Viewer.h>
 #include <Eigen/Core>
 
@@ -7,19 +14,42 @@
 int main(int argc, char *argv[])
 {
   // Scale for the color axis
-  double scale = 1.0;
+  double scale = 100.0;
   Eigen::MatrixXd V;
   Eigen::MatrixXi F;
   // Load input meshes
   igl::read_triangle_mesh(
     (argc>1?argv[1]:"../shared/data/cactus.obj"),V,F);
+
+  Eigen::SparseMatrix<double> M;
+  igl::massmatrix(V,F,igl::MASSMATRIX_TYPE_DEFAULT,M);
+  Eigen::VectorXd A = M.diagonal();
+
+  Eigen::VectorXd D,G,H,K1,K2;
+  Eigen::MatrixXd D1,D2;
+  // Angle defect ~ locally integrated Gaussian curvature
+  angle_defect(V,F,D);
+  // average locally (i.e., "un-integrate" to pointwise quantity for
+  // visualization)
+  G = D.array()/A.array();
+  mean_curvature(V,F,H);
+  principal_curvatures(V,F,D1,D2,K1,K2);
+
   igl::viewer::Viewer viewer;
   std::cout<<R"(
 S,s      Stretch, squish color axis range
+G        Show Gaussian curvature (using principal_curvatures)
+g        Show Gaussian curvature (using angle_defect)
+M        Show Gaussian curvature (using principal_curvatures)
+m        Show discrete mean curvature (using mean_curvature)
+K        Show maximum curvature (using principal_curvatures)
+k        Show minimum curvature (using principal_curvatures)
+D,d      Show principal directions 
 )";
+  // Default to mean curvature
+  Eigen::VectorXd Z = H;
   const auto update = [&]()
   {
-    Eigen::VectorXd Z = V.col(1);
     Eigen::MatrixXd C;
     igl::parula(Z,-scale,scale,C);
     viewer.data.set_colors(C);
@@ -29,11 +59,47 @@ S,s      Stretch, squish color axis range
   {
     switch(key)
     {
+      case 'D':
+      case 'd':
+        viewer.core.show_overlay ^= 1;
+        break;
+      case 'G':
+        Z = K1.array()*K2.array();
+        break;
+      case 'g':
+        Z = G;
+        break;
+      case 'K':
+        Z = K1;
+        break;
+      case 'k':
+        Z = K2;
+        break;
+      case 'M':
+        Z = 0.5*(K1+K2);
+        break;
+      case 'm':
+        Z = H;
+        break;
       case 'S':
       case 's':
         scale *= key=='S' ? 2.0 : 0.5;
         std::cout<<"Color axis range: ["<<-scale<<","<<scale<<"]"<<std::endl;
         break;
+      case ' ':
+      {
+        const int width=2560;
+        const int height = 
+          (viewer.core.viewport(3)/ viewer.core.viewport(2)) * width;
+        Eigen::Matrix<unsigned char,Eigen::Dynamic,Eigen::Dynamic> 
+          R(width,height), G(width,height), B(width,height), A(width,height);
+        viewer.core.draw_buffer(viewer.data,viewer.opengl,false,R,G,B,A);
+        A.setConstant(255);
+        const std::string prefix = std::string(argv[0])+"-capture-";
+        std::string filename;
+        igl::next_filename(prefix,4,".png",filename);
+        igl::png::writePNG(R,G,B,A,filename);
+      }
       default:
         return false;
     }
@@ -42,9 +108,30 @@ S,s      Stretch, squish color axis range
   };
 
   viewer.data.set_mesh(V,F);
+  Eigen::MatrixXd lP(V.rows()*4,3);
+  const double h = igl::avg_edge_length(V,F);
+  lP << V-0.5*h*D1, V+0.5*h*D1, V-0.5*h*D2, V+0.5*h*D2;
+  Eigen::MatrixXi lE(2*V.rows(),2);
+  Eigen::MatrixXd lC(2*V.rows(),3);
+
+  const Eigen::RowVector3d orange(1.0,0.7,0.2);
+  const Eigen::RowVector3d yellow(1.0,0.9,0.2);
+  const Eigen::RowVector3d blue(0.2,0.3,0.8);
+  for(int e = 0;e<V.rows();e++)
+  {
+    lE(e,0)          = e+0*V.rows();
+    lE(e,1)          = e+1*V.rows();
+    lE(V.rows()+e,0) = e+2*V.rows();
+    lE(V.rows()+e,1) = e+3*V.rows();
+    lC.row(         e) = orange;
+    lC.row(V.rows()+e) = blue;
+  }
+  viewer.data.set_edges(lP,lE,lC);
+
   update();
   viewer.core.show_lines = false;
-  viewer.data.face_based = true;
+  viewer.core.show_overlay = false;
+  viewer.data.face_based = false;
   viewer.launch();
   return EXIT_SUCCESS;
 }
