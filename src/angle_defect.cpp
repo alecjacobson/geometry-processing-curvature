72e9cdf595e0dda7cc2e247acccb4f99ad30b9a1
rkevingibson <rkevingibson@gmail.com>
2017-03-17 15:45:32 -0400

    First draft of everything. Some issue with mean curvature maybe?

--- src/angle_defect.cpp
+++ src/angle_defect.cpp
@@ -1,9 +1,21 @@
 #include "../include/angle_defect.h"
+#include "internal_angles.h"
+#include <igl/squared_edge_lengths.h>
 
 void angle_defect(
-  const Eigen::MatrixXd & V,
-  const Eigen::MatrixXi & F,
-  Eigen::VectorXd & D)
+	const Eigen::MatrixXd & V,
+	const Eigen::MatrixXi & F,
+	Eigen::VectorXd & D)
 {
-  D = Eigen::VectorXd::Zero(V.rows());
+	Eigen::MatrixXd l_sqr;
+	igl::squared_edge_lengths(V, F, l_sqr);
+	Eigen::MatrixXd angles;
+	internal_angles(l_sqr, angles);
+	D = Eigen::VectorXd::Constant(V.rows(), 2 * M_PI);
+
+	for (int f = 0; f < F.rows(); f++) {
+		D(F(f, 0)) -= angles(f, 0);
+		D(F(f, 1)) -= angles(f, 1);
+		D(F(f, 2)) -= angles(f, 2);
+	}
 }
--- src/internal_angles.cpp
+++ src/internal_angles.cpp
@@ -1,8 +1,22 @@
 #include "../include/internal_angles.h"
+#include <cmath>
 
 void internal_angles(
-  const Eigen::MatrixXd & l_sqr,
-  Eigen::MatrixXd & A)
+	const Eigen::MatrixXd & l_sqr,
+	Eigen::MatrixXd & A)
 {
-  // Add with your code
+	// Add with your code
+	auto num_faces = l_sqr.rows();
+
+	const Eigen::MatrixXd l = l_sqr.cwiseSqrt();
+
+	A.resize(num_faces, 3);
+	for (int f = 0; f < num_faces; f++) {
+		for (int v = 0; v < 3; v++) {
+			double numerator = l_sqr(f, (v + 1) % 3) + l_sqr(f, (v + 2) % 3) - l_sqr(f, v);
+			double denominator = 2 * l(f, (v + 1) % 3)*l(f, (v + 2) % 3);
+			A(f, v) = acos(numerator / denominator);
+		}
+	}
+
 }
