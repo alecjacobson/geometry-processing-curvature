a1e8835f4573643b27ca8ad19cf63921949331d6
Michael Tao <mtao@dgp.toronto.edu>
2017-03-18 18:39:41 -0400

    mean curvaturet works (because i'm comparing with normals and not doing abs(H) like i was before

--- src/mean_curvature.cpp
+++ src/mean_curvature.cpp
@@ -1,21 +1,26 @@
 #include "../include/mean_curvature.h"
 #include <igl/cotmatrix.h>
 #include <igl/massmatrix.h>
+#include <igl/per_vertex_normals.h>
 
 void mean_curvature(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::VectorXd & H)
 {
+    Eigen::MatrixXd N;
     Eigen::SparseMatrix<double> L,M;
     H = Eigen::VectorXd::Zero(V.rows());
     igl::cotmatrix(V,F,L);
     L = -L;
+    igl::per_vertex_normals(V,F,N);
 
     igl::massmatrix(V,F,igl::MASSMATRIX_TYPE_DEFAULT, M);
     for(int i = 0; i < M.rows(); ++i) {
         auto&& v = M.coeffRef(i,i);
         v = 1.0/v;
     }
-    H = .5 * (M * L * V).rowwise().norm();
+    H = ((.5 * M * L * V).array() * N.array()).rowwise().sum();
+    std::cout << "Mean curvature!" << std::endl;
+    std::cout << H.transpose() << std::endl;
 }
