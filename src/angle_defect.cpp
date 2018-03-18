9cfcf2d3a44dd3d349df28159e2e4cecbec98993
zero-impact <lawsonfulton@gmail.com>
2018-03-18 19:48:09 -0400

    done

--- src/angle_defect.cpp
+++ src/angle_defect.cpp
@@ -1,9 +1,25 @@
 #include "../include/angle_defect.h"
+#include "../include/internal_angles.h"
+
+#include <igl/squared_edge_lengths.h>
 
 void angle_defect(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::VectorXd & D)
 {
-  D = Eigen::VectorXd::Zero(V.rows());
+  D = Eigen::VectorXd::Ones(V.rows()) * 2.0 * M_PI;
+
+  Eigen::MatrixXd l_sqr;
+  igl::squared_edge_lengths(V, F, l_sqr);
+
+  Eigen::MatrixXd A;
+  internal_angles(l_sqr, A);
+
+  for(int i = 0; i < F.rows(); i++) {
+    for(int j = 0; j < 3; j++) {
+      int v_i = F(i, j);
+      D[v_i] -= A(i, j);
+    }
+  }
 }
