41de27f24d7d8bcbca8e68bfcac7e0d7a3b4224c
Ziheng Liang <ziheng941008@hotmail.com>
2018-03-19 17:26:12 -0400

    finished mean

--- src/mean_curvature.cpp
+++ src/mean_curvature.cpp
@@ -1,10 +1,18 @@
 #include "../include/mean_curvature.h"
+#include <igl/cotmatrix.h>
+#include <igl/massmatrix.h>
+#include <iostream>
 
 void mean_curvature(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::VectorXd & H)
 {
-  // Replace with your code
-  H = Eigen::VectorXd::Zero(V.rows());
+	std::cout << "test" << std::endl;
+	Eigen::SparseMatrix<double> L; 
+	igl::cotmatrix(V, F, L);
+	Eigen::SparseMatrix<double> M;
+	igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_BARYCENTRIC, M);
+	H = (M.cwiseInverse() * L * V).rowwise().squaredNorm();
+	std::cout << "test2" << std::endl;
 }
