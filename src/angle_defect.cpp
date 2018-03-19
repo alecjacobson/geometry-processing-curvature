3f896106d761d7774bb766974c5fca6cd038b69e
HTDerekLiu <htliu1992@gmail.com>
2018-03-18 23:30:29 -0400

    finish

--- src/angle_defect.cpp
+++ src/angle_defect.cpp
@@ -1,9 +1,26 @@
 #include "../include/angle_defect.h"
+#include "../include/internal_angles.h"
+#include <igl/squared_edge_lengths.h>
+#include <igl/PI.h>
+
+using namespace std;
 
 void angle_defect(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::VectorXd & D)
 {
-  D = Eigen::VectorXd::Zero(V.rows());
+	Eigen::MatrixXd edgeLengthSquare;
+	igl::squared_edge_lengths(V, F, edgeLengthSquare);
+
+	Eigen::MatrixXd internalAngles;
+	internal_angles(edgeLengthSquare, internalAngles);
+
+	D = Eigen::VectorXd::Zero(V.rows());
+	D.array() += 2 * igl::PI;
+	for(int ii = 0; ii < F.rows(); ii++){
+		D(F(ii,0)) -= internalAngles(ii,0);
+		D(F(ii,1)) -= internalAngles(ii,1);
+		D(F(ii,2)) -= internalAngles(ii,2);
+  }
 }
--- src/internal_angles.cpp
+++ src/internal_angles.cpp
@@ -5,4 +5,13 @@ void internal_angles(
   Eigen::MatrixXd & A)
 {
   // Add with your code
+  Eigen::ArrayXd aSquare, bSquare, cSquare, cosTheta;
+	A.resize(l_sqr.rows(), l_sqr.cols());
+	for (int ii = 0; ii < l_sqr.cols(); ii++){
+		aSquare = l_sqr.col((ii+1)%3).array();
+		bSquare = l_sqr.col((ii+2)%3).array();
+		cSquare = l_sqr.col((ii  )%3).array();
+		cosTheta = (aSquare + bSquare - cSquare) / (2*aSquare.sqrt()*bSquare.sqrt());
+		A.col(ii) = cosTheta.acos();
+	}
 }
