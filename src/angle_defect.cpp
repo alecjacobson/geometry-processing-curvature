7f0416c09886e28fc98a9bf33af5a9593d1c687e
Michelle <m.arkhangorodsky@gmail.com>
2018-03-20 11:09:47 -0400

    implemented angle defect.. looks a lil funny

--- src/angle_defect.cpp
+++ src/angle_defect.cpp
@@ -1,9 +1,29 @@
 #include "../include/angle_defect.h"
+#include "igl/squared_edge_lengths.h"
+#include "internal_angles.h"
 
 void angle_defect(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::VectorXd & D)
 {
-  D = Eigen::VectorXd::Zero(V.rows());
+  D = Eigen::VectorXd::Constant(V.rows(), 2 * M_PI);
+
+  Eigen::MatrixXd l_sqr;
+  igl::squared_edge_lengths(V, F, l_sqr);
+
+  Eigen::MatrixXd A;
+  internal_angles(l_sqr, A);
+
+  int num_F = A.rows();
+  for(int f_idx = 0; f_idx < num_F; f_idx++){
+    // indices into V
+    int v_i = F(f_idx, 0);
+    int v_j = F(f_idx, 1);
+    int v_k = F(f_idx, 2);
+
+    D(v_i) -= A(f_idx, 0);
+    D(v_j) -= A(f_idx, 1);
+    D(v_k) -= A(f_idx, 2);
+  }
 }
--- src/internal_angles.cpp
+++ src/internal_angles.cpp
@@ -1,4 +1,5 @@
 #include "../include/internal_angles.h"
+#include <iostream>
 
 void internal_angles(
   const Eigen::MatrixXd & l_sqr,
@@ -6,11 +7,13 @@ void internal_angles(
 {
   // Add with your code
   int num_faces = l_sqr.rows();
+  A.resize(num_faces, 3);
   for(int f_idx = 0; f_idx < num_faces; f_idx++){
     for(int i=0; i < 3; i++){
       double a_squared = l_sqr(f_idx,i);
       double b_squared = l_sqr(f_idx,(i+1)%3);
       double c_squared = l_sqr(f_idx,(i+2)%3);
+      // acos is in radians
       A(f_idx, i) = acos(
                     (b_squared + c_squared - a_squared)
                     / (2 * sqrt(c_squared) * sqrt(a_squared)));
