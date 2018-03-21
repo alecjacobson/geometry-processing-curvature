968aa5721bbfdb0328d9ad07ee06c30b7d1e817d
Sarah Kushner <sarahannekushner@yahoo.com>
2018-03-21 03:57:25 -0400

    only 4 hours late :/

--- src/principal_curvatures.cpp
+++ src/principal_curvatures.cpp
@@ -1,6 +1,7 @@
 #include "../include/principal_curvatures.h"
 #include <Eigen/Dense>
 #include <vector>
+#include <igl/per_vertex_normals.h>
 #include <igl/slice.h>
 #include <igl/pinv.h>
 
@@ -19,6 +20,9 @@ void principal_curvatures(
   D2 = Eigen::MatrixXd::Zero(V.rows(),3);
 
   Eigen::MatrixXd P;
+  Eigen::MatrixXd N;
+  igl::per_vertex_normals(V, F, N);
+
   Eigen::VectorXi C(3);
   C(0) = 0;
   C(1) = 1;
@@ -33,13 +37,17 @@ void principal_curvatures(
 	  for (int j = 0; j < F.rows(); j++)
 	  {
 		  auto face = F.row(j);
-		  if((face.array() == i).any())
+		  if ((face.array() == i).any())
 		  {
 			  r1.push_back(face(0));
 			  r1.push_back(face(1));
 			  r1.push_back(face(2));
 		  }
-		  // two-ring
+	  }
+	  // two-ring
+	  for (int j = 0; j < F.rows(); j++)
+	  {
+		  auto face = F.row(j);
 		  for (int k = 0; k < r1.size(); k++)
 		  {
 			  if ((face.array() == r1[k]).any())
@@ -49,42 +57,47 @@ void principal_curvatures(
 				  r2.push_back(face(2));
 			  }
 		  }
-		  //std::cout << j << std::endl;
 	  }
 
 	  r1.reserve(r1.size() + r2.size());
 	  r1.insert(r1.end(), r2.begin(), r2.end());
 	  
 	  std::sort(r1.begin(), r1.end());
+	  r1.erase(std::remove(r1.begin(), r1.end(), i), r1.end());
 	  r1.erase(unique(r1.begin(), r1.end()), r1.end());
-	  /*
-	  int *arr = &r1[r1.size()];
-	  Eigen::Map<Eigen::MatrixXi> R(arr, r1.size(), 1);
+	  
+	  int* arr = &r1[0];
+	  Eigen::Map<Eigen::VectorXi> R(arr, r1.size(), 1);
+	  
 	  igl::slice(V, R, C, P);
 	  
+	  P = P.rowwise() - V.row(i);
 	  
 	  Eigen::Matrix3d Pt_P = P.transpose() * P;
-	  std::cout << Pt_P.rows() << " " << Pt_P.cols() << std::endl;
 	  
 	  Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> eigensolver(Pt_P);
 	  Eigen::MatrixXd p = eigensolver.eigenvectors();
-	  std::cout << eigensolver.eigenvalues() << std::endl;
-
-	  Eigen::VectorXd u = p.col(0);
-	  Eigen::VectorXd v = p.col(1);
-	  Eigen::VectorXd w = p.col(2);
+	  //std::cout << eigensolver.eigenvalues() << std::endl;
 	  
-	  Eigen::MatrixXd H(5, 3);
-	  H.col(0) = u;
-	  H.col(1) = v;
-	  H.col(2) = u*u;
-	  H.col(3) = u*v;
-	  H.col(4) = v*v;
+	  Eigen::VectorXd u = p.col(2);
+	  Eigen::VectorXd v = p.col(1);
+	  Eigen::VectorXd w = p.col(0);
+	  
+	  Eigen::MatrixXd H(P.rows(),5);
+	  H.col(0) = P * u;
+	  H.col(1) = P * v;
+	  H.col(2) = H.col(0).cwiseProduct(H.col(0));
+	  H.col(3) = H.col(0).cwiseProduct(H.col(1));
+	  H.col(4) = H.col(1).cwiseProduct(H.col(1));
 	  
 	  Eigen::MatrixXd X;
 	  igl::pinv(H, X);
-	  Eigen::VectorXd a = X*w;
-	  std::cout << a.cols() << " " << a.rows() << std::endl;
+
+	  float dot = w.dot(N.row(i));
+	  signbit(dot) ? w = -w : w = w;
+
+	  Eigen::VectorXd a = X * P * w;
+
 	  double e = (2 * a(2)) / (sqrt(pow(a(0), 2) + 1 + pow(a(1), 2)));
 	  double f = a(3) / (sqrt(pow(a(0), 2) + 1 + pow(a(1), 2)));
 	  double g = (2 * a(4)) / (sqrt(pow(a(0), 2) + 1 + pow(a(1), 2)));
@@ -98,13 +111,17 @@ void principal_curvatures(
 	  Eigen::Matrix2d s2;
 	  s2 << E, F,
 			F, G;
-	  Eigen::MatrixXd S = -s1 * s2.inverse();
-	  Eigen::JacobiSVD<Eigen::MatrixXd> svd(S, Eigen::ComputeThinU | Eigen::ComputeThinV);
-	  K1(i) = svd.singularValues()(0);
-	  K2(i) = svd.singularValues()(1);
-	  D1.row(i) = svd.matrixU().col(0) * u + svd.matrixU().col(1) * v;
-	  D2.row(i) = svd.matrixV().col(0) * u + svd.matrixV().col(1) * v;
-	  */
+	  Eigen::Matrix2d S = -s1 * s2.inverse();
+
+	  Eigen::SelfAdjointEigenSolver<Eigen::Matrix2d> eigensolver_shape(S);
+	  Eigen::MatrixXd s = eigensolver_shape.eigenvectors();
+
+	  K1(i) = eigensolver_shape.eigenvalues()(1);
+	  K2(i) = eigensolver_shape.eigenvalues()(0);
+
+	  D1.row(i) = s(1, 1) * u + s(1, 0) * v;
+	  D1.row(i) = s(0, 1) * u + s(0, 0) * v;
+	  
   }
 
 }
