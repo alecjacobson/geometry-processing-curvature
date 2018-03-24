dd7bacf39ae1626596432be96a4025ae5bd3093e
sajjadn <nezhadi.sajjad@gmail.com>
2018-03-24 01:34:53 -0400

    last asmt

--- src/angle_defect.cpp
+++ src/angle_defect.cpp
@@ -1,4 +1,7 @@
 #include "../include/angle_defect.h"
+#include "../include/internal_angles.h"
+#include <igl/squared_edge_lengths.h>
+#include <iostream>
 
 void angle_defect(
   const Eigen::MatrixXd & V,
@@ -6,4 +9,19 @@ void angle_defect(
   Eigen::VectorXd & D)
 {
   D = Eigen::VectorXd::Zero(V.rows());
+  Eigen::MatrixXd K, L;
+
+  igl::squared_edge_lengths(V, F, L);
+  internal_angles(L, K);
+
+   for (int i = 0; i< V.rows(); i++){
+    D(i) = 2*M_PI;
+   }
+   
+   for (int i = 0; i < F.rows(); i++) {
+    for(int j= 0; j < 3; j++){
+        D(F(i,j)) -= K(i, j);
+      }
+    }
+    std::cout << D << std::endl;
 }
--- src/mean_curvature.cpp
+++ src/mean_curvature.cpp
@@ -1,4 +1,9 @@
 #include "../include/mean_curvature.h"
+#include <igl/invert_diag.h>
+#include <igl/per_vertex_normals.h>
+#include <igl/cotmatrix.h>
+#include <igl/massmatrix.h>
+#include <iostream>
 
 void mean_curvature(
   const Eigen::MatrixXd & V,
@@ -7,4 +12,20 @@ void mean_curvature(
 {
   // Replace with your code
   H = Eigen::VectorXd::Zero(V.rows());
+  Eigen::SparseMatrix<double> L;
+  Eigen::SparseMatrix<double> M;
+  Eigen::SparseMatrix<double> Y;
+  Eigen::MatrixXd N;
+
+  igl::cotmatrix(V, F, L); 
+  igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_DEFAULT, M);
+  igl::invert_diag(M, Y);
+  igl::per_vertex_normals(V, F, N);
+  std::cout << Y.cols() << std::endl;
+  std::cout << L.rows() << std::endl;
+  std::cout << V.rows() << std::endl;
+
+  Eigen::MatrixXd temp = Y * L * V * N.transpose();
+  for (int i = 0; i < V.rows(); i++)
+    H(i) = -temp(i,i);
 }
