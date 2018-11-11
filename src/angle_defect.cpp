efa568d77eb0ad85f60644fa601b14052f85f464
Zhiyuan <jerryz.cheng@mail.utoronto.ca>
2018-11-10 21:53:28 -0500

    submit

--- src/angle_defect.cpp
+++ src/angle_defect.cpp
@@ -1,9 +1,25 @@
 #include "../include/angle_defect.h"
+#include "../include/internal_angles.h"
+#include <igl/squared_edge_lengths.h>
+// find value for pi
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 void angle_defect(
-  const Eigen::MatrixXd & V,
-  const Eigen::MatrixXi & F,
-  Eigen::VectorXd & D)
+  	const Eigen::MatrixXd & V,
+  	const Eigen::MatrixXi & F,
+  	Eigen::VectorXd & D)
 {
-  D = Eigen::VectorXd::Zero(V.rows());
+	// compute angles
+	Eigen::MatrixXd l_sqr, A;
+	igl::squared_edge_lengths(V, F, l_sqr);
+	internal_angles(l_sqr, A);
+
+  	D = Eigen::VectorXd::Ones(V.rows());
+  	D = 2 * M_PI * D;
+  	for (int i = 0; i < F.rows(); i++) {
+  		for (int j = 0; j < 3; j++) {
+  			D(F(i, j)) -= A(i, j);
+  		}
+  	}
 }
