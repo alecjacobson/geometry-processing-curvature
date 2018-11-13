77f3b1bfe95154ea62d47e5d81244b1f0dd5f50e
Mark <peiqi1122@gmail.com>
2018-11-13 02:02:42 -0500

    angle defect

--- src/angle_defect.cpp
+++ src/angle_defect.cpp
@@ -1,9 +1,29 @@
 #include "../include/angle_defect.h"
+#include "../include/internal_angles.h"
+
+#include <igl/squared_edge_lengths.h>
 
 void angle_defect(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::VectorXd & D)
 {
-  D = Eigen::VectorXd::Zero(V.rows());
+    // Interior angles
+
+    Eigen::MatrixXd l_sqr, A;
+    igl::squared_edge_lengths(V, F, l_sqr);
+    internal_angles(l_sqr, A);
+
+    // Compute angle defect
+    //      \Sigma_{i} = 2\pi - \sum_{f\in F(i)} InteriorAngle(i, f)
+
+    D = Eigen::VectorXd::Ones(V.rows());
+    D = 2 * M_PI * D;
+
+    for (int i = 0; i < F.rows(); ++i) {
+        for (int j = 0; j < 3; ++j) {
+            auto v = F(i, j);
+            D(v) = D(v) - A(i, j);
+        }
+    }
 }
--- src/internal_angles.cpp
+++ src/internal_angles.cpp
@@ -1,8 +1,29 @@
 #include "../include/internal_angles.h"
 
+#include <cmath>
+
 void internal_angles(
   const Eigen::MatrixXd & l_sqr,
   Eigen::MatrixXd & A)
 {
-  // Add with your code
+    A.resizeLike(l_sqr);
+
+    // return angle opposite of edge `c`
+    auto angle = [](double a2, double b2, double c2) {
+        return std::acos((a2 + b2 - c2) / (2 * sqrt(a2) * sqrt(b2)));
+    };
+
+    double a2, b2, c2;
+    for (int i = 0; i < l_sqr.rows(); ++i) 
+    {
+        // row = [A, B, C] where `A` opposite of edge `a`
+
+        a2 = l_sqr(i, 0);
+        b2 = l_sqr(i, 1);
+        c2 = l_sqr(i, 2);
+
+        A(i, 0) = angle(b2, c2, a2);
+        A(i, 1) = angle(c2, a2, b2);
+        A(i, 2) = angle(a2, b2, c2);
+    }
 }
