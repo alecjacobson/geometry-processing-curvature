dac4cabfa5077c651ed89ccf48515c09fc466495
Randy Hickey <randyhickey@Randy.local>
2018-11-13 23:41:48 -0500

    Finished

--- src/angle_defect.cpp
+++ src/angle_defect.cpp
@@ -1,9 +1,21 @@
 #include "../include/angle_defect.h"
+#include "igl/squared_edge_lengths.h"
+#include "internal_angles.h"
+#include "igl/PI.h"
 
 void angle_defect(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::VectorXd & D)
 {
-  D = Eigen::VectorXd::Zero(V.rows());
+    Eigen::MatrixXd l_sqr, X;
+    igl::squared_edge_lengths(V, F, l_sqr);
+    internal_angles(l_sqr, X);
+    
+    D = 2 * igl::PI * Eigen::VectorXd::Ones(V.rows());
+    for (int i = 0; i < F.rows(); i++) {
+        D(F(i, 0)) += -X(i, 0);
+        D(F(i, 1)) += -X(i, 1);
+        D(F(i, 2)) += -X(i, 2);
+    }
 }
--- src/mean_curvature.cpp
+++ src/mean_curvature.cpp
@@ -1,10 +1,30 @@
 #include "../include/mean_curvature.h"
+#include "igl/cotmatrix.h"
+#include "igl/massmatrix.h"
+#include "igl/invert_diag.h"
+#include "igl/per_vertex_normals.h"
 
 void mean_curvature(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::VectorXd & H)
 {
-  // Replace with your code
-  H = Eigen::VectorXd::Zero(V.rows());
+    Eigen::SparseMatrix<double> L, M, M_i;
+    Eigen::MatrixXd curve_normals, normals;
+    igl::cotmatrix(V, F, L);
+    igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_VORONOI, M);
+    igl::invert_diag(M, M_i);
+    curve_normals = (M_i * L) * V;
+    igl::per_vertex_normals(V, F, normals);
+    
+    H.resize(curve_normals.rows());
+    for (int i = 0; i < V.rows(); i++) {
+        H(i) = 0.0;
+        if ((normals.row(i)).dot(curve_normals.row(i)) > 0) {
+            H(i) = (curve_normals.row(i)).norm();
+        }
+        else if ((normals.row(i)).dot(curve_normals.row(i)) < 0) {
+            H(i) = - ((curve_normals.row(i)).norm());
+        }
+    }
 }
