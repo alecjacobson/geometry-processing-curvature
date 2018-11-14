f0023f517050e01d2d21ec077c4e6cbded76b267
liupeiLu <liupei.lu@mail.utoronto.ca>
2018-11-14 01:18:46 -0500

    HW06 Liupei Lu

--- src/angle_defect.cpp
+++ src/angle_defect.cpp
@@ -1,9 +1,24 @@
 #include "../include/angle_defect.h"
+#include "../include/internal_angles.h"
+#include <igl/squared_edge_lengths.h>
+#include <cmath>
 
 void angle_defect(
-  const Eigen::MatrixXd & V,
-  const Eigen::MatrixXi & F,
-  Eigen::VectorXd & D)
+    const Eigen::MatrixXd &V,
+    const Eigen::MatrixXi &F,
+    Eigen::VectorXd &D)
 {
-  D = Eigen::VectorXd::Zero(V.rows());
+  //D = Eigen::VectorXd::Zero(V.rows());
+  Eigen::MatrixXd L, A;
+  igl::squared_edge_lengths(V, F, L);
+  internal_angles(L, A);
+
+  //2pi - A
+  D.resize(V.rows());
+  for (int i = 0; i < F.rows(); i++)
+  {
+    D(F(i, 0)) = 2.00 * M_PI - A(i, 0);
+    D(F(i, 1)) = 2.00 * M_PI - A(i, 1);
+    D(F(i, 2)) = 2.00 * M_PI - A(i, 2);
+  }
 }
