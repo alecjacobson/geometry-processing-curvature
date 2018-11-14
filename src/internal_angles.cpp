4f8fb3ef064d662380f14976ea348f5387028732
Feilong Song <feilong.song@mail.utoronto.ca>
2018-11-14 00:02:34 -0500

    hw6

--- src/internal_angles.cpp
+++ src/internal_angles.cpp
@@ -5,4 +5,13 @@ void internal_angles(
   Eigen::MatrixXd & A)
 {
   // Add with your code
+  A = Eigen::MatrixXd::Zero(l_sqr.rows(), l_sqr.cols());
+  for (int m = 0; m < A.rows(); m++) {
+    for (int i = 0; i < 3; i++) {
+      double l1 = l_sqr(m, i);
+      double l2 = l_sqr(m, (i + 1) % 3);
+      double l3 = l_sqr(m, (i + 2) % 3);
+      A(m, i) = acos((l3 + l2 - l1) / (2.0 * sqrt(l3 * l2)));
+    }
+  }
 }
--- src/mean_curvature.cpp
+++ src/mean_curvature.cpp
@@ -1,4 +1,9 @@
 #include "../include/mean_curvature.h"
+#include <Eigen/Sparse>
+#include <igl/massmatrix.h>
+#include <igl/cotmatrix.h>
+#include <igl/invert_diag.h>
+#include <igl/per_vertex_normals.h>
 
 void mean_curvature(
   const Eigen::MatrixXd & V,
@@ -6,5 +11,19 @@ void mean_curvature(
   Eigen::VectorXd & H)
 {
   // Replace with your code
-  H = Eigen::VectorXd::Zero(V.rows());
+  Eigen::SparseMatrix<double> L, M, Mi;
+  igl::cotmatrix(V, F, L);
+  igl::massmatrix(V, F, igl::MassMatrixType::MASSMATRIX_TYPE_DEFAULT, M);
+  igl::invert_diag(M, Mi);
+
+  Eigen::MatrixXd N;
+  igl::per_vertex_normals(V, F, N);
+
+  Eigen::MatrixXd MLV = Mi * L * V;
+
+  H.resize(MLV.rows());
+  for (int i = 0; i < H.size(); i++) {
+    int sign = (MLV.row(i).dot(N.row(i)) >= 0) ? 1 : -1;
+    H(i) = sign * MLV.row(i).norm();
+  }
 }
